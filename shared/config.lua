638ab3e39abe08f76c6a20f4f858a442dc492b03
Maximus7474 <the.maximus.7474@gmail.com>
2024-07-24 14:27:38 +0200

    rf(config): added comment to guide users

--- shared/config.lua
+++ shared/config.lua
@@ -42,26 +42,26 @@ Config.Options = {
 
 Config.Elevators = {
     ["Maze Bank Tower"] = {
-        restricted = false,
+        restricted = { ["police"] = 0 }, --[[ or false to disable ]]
         vehicle = false,
         floors = {
             {
                 floor = "-2",
                 label = "Garage Entrance",
                 panel = vector4(-82.5941, -819.1429, 36.0281, 260.1323),
-                position = vector4(-84.2893, -821.5383, 36.0281, 344.3796)
+                position = vector4(-84.2893, -821.5383, 36.0281 - 0.98, 344.3796)
             },
             {
                 floor = "57",
                 label = "Interior Garage",
                 panel = vector4(-92.1781, -821.9614, 222.0004, 251.2057),
-                position = vector4(-91.8228, -821.1077, 222.0004, 255.4299)
+                position = vector4(-88.2165, -821.2771, 222.0004 - 0.98, 254.0536)
             },
             {
                 floor = "65",
                 label = "Mod Shop",
                 panel = vector4(-73.5848, -809.6722, 285.0000, 163.1215),
-                position = vector4(-71.4348, -810.7918, 285.0000, 159.1122)
+                position = vector4(-73.3622, -813.9121, 284.9999 - 0.98, 163.1935)
             },
         }
     }
