c7e0847e54cfc544b1c7fa20913aa11402c80359
Maximus7474 <the.maximus.7474@gmail.com>
2024-10-27 23:31:14 +0100

    rf(utils): return formatted floors and if any restrictions

--- client/modules/utils.lua
+++ client/modules/utils.lua
@@ -1,18 +1,26 @@
+local Framework = require (('client.modules.frameworks.%s'):format(GetFrameworkRequirePath()))
+
 local Utils = {}
 
 ---Convert Config file structure to NUI structure
 ---@param floors table
----@return table formattedFloors
+---@return table, boolean
 function Utils.FormatFloors(floors)
     local formattedFloors = {}
+    local restricted = false
     for i = 1, #floors do
+        local floor = floors[i];
+
+        if not restricted and floor.restrictions then restricted = true end
+
         formattedFloors[i] = {
             index = i,
-            floor = floors[i].floor,
-            label = floors[i].label,
+            floor = floor.floor,
+            label = floor.label,
+            hasAccess = floor.restrictions and Framework:HasGroup(floor.restrictions) or true
         }
     end
-    return formattedFloors
+    return formattedFloors, restricted
 end
 
 return Utils
